d6105f80caac2ca0e409e81f6a756e32a2ffc2d9
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2024-07-14 02:40:00 +0100

    Complete anecdotes task

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,70 +1,66 @@
-const Header = (props) => {
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Votes = ({ anecdotes, votes }) => {
+  const mostVotes = Math.max(...votes);
+  const index = votes.indexOf(mostVotes);
+
   return (
     <div>
-      <h1>{props.course.name}</h1>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[index]}</div>
+      <div>has {mostVotes} votes</div>
     </div>
   );
 };
 
-const Part = (props) => {
+const Anecdote = ({ anecdotes, selected, votes }) => {
   return (
     <div>
-      <p>{props.data.name} {props.data.exercises}</p>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
     </div>
   );
 };
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part data={props.course.parts[0]}/>
-      <Part data={props.course.parts[1]}/>
-      <Part data={props.course.parts[2]}/>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises {
-          props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
-        }
-      </p>
-    </div>
-  );
-}
-
-
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {       
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14, 
-      },    
-    ],
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const random = () => Math.floor(Math.random() * anecdotes.length);
+
+  const voting = () => {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+    console.log(votesCopy);
   };
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      <Anecdote anecdotes={anecdotes} selected={selected} votes={votes} />
+      <Button handleClick={voting} text={"vote"} />
+      <Button
+        handleClick={() => setSelected(random())}
+        text={"next anecdote"}
+      />
+      <Votes anecdotes={anecdotes} votes={votes} />
     </div>
   );
-
 };
 
 export default App;
